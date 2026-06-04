c50a96480882140adb6d842bb0b35181c563d7d1
Rakthesh <raktheshtg@example.com>
2026-06-05 02:45:09 +0530

    fix: remove deprecated manual db connect block

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -30,13 +30,6 @@ db.query('SELECT 1', (err, results) => {
     }
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("DB Connection Failed:", err);
-  } else {
-    console.log("Connected to MySQL Database");
-  }
-});
 
 
 
